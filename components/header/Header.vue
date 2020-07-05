680f2deecf963d641129623b6f5e1cc10bba3b70
ruslanzamalov <zamruma@gmail.com>
2020-07-05 16:46:03 +0300

    header section and module navigation, remove rd from assets

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,8 +1,39 @@
 <template>
-  <header>
-    <Nav/>
-    <button>
-
-    </button>
+  <header class="menu">
+    <div class="menu__box wrap">
+      <div class="menu__logo">
+        <nuxt-link exact active-class="links--active" class="links" to="/">
+          Home Page
+        </nuxt-link>
+      </div>
+      <!--<button class="menu__button" :class="{ 'menu__button&#45;&#45;active': isActive.default }" @click="openMenu">-->
+      <!--Go-->
+      <!--</button>-->
+      <Nav/>
+    </div>
+    <!--<Nav :is-active="isActive"/>-->
   </header>
 </template>
+
+<script>
+import Nav from '~/components/header/Nav'
+
+export default {
+  components: {
+    Nav
+  }
+  // data () {
+  //   return {
+  //     isActive: {
+  //       type: Boolean,
+  //       default: false
+  //     }
+  //   }
+  // },
+  // methods: {
+  //   openMenu () {
+  //     this.isActive.default = !this.isActive.default
+  //   }
+  // }
+}
+</script>
--- components/header/Nav.vue
+++ components/header/Nav.vue
@@ -1,7 +1,18 @@
 <template>
-  <nav>
-    <ul>
-      <li></li>
+<!--  <nav class="nav" :class="{ 'nav&#45;&#45;active': isActive.default }">-->
+    <nav class="nav">
+    <ul class="nav__items">
+      <li class="nav__item">
+        <nuxt-link to="/contacts" active-class="links--active" class="links">
+          Contacts
+        </nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  // props: ['isActive']
+}
+</script>
