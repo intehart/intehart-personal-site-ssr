ea5e8f83cfd0101f89601565e159edeaf1d6b613
ruslanzamalov <zamruma@gmail.com>
2020-07-08 21:35:35 +0300

    preview section - init

---
+++ components/preview/Preview.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="preview">
+    <div class="preview__top">
+      <div class="wrap">
+        top
+      </div>
+    </div>
+    <div class="wrap wrap--relative">
+      <div class="preview__center">
+        <span class="preview__first-name">Ruslan</span>
+        <span class="preview__last-name">Zamalov</span>
+      </div>
+    </div>
+    <div class="preview__bottom">
+      <div class="wrap">
+        bottom
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
